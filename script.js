a493a982e296a422bf0c9917174ef9fcb3fe5a5f
Akaryuu9 <a.mocalovaite@gmail.com>
2023-02-20 20:44:10 +0200

    improved readability, added script.js

---
+++ script.js
@@ -0,0 +1,67 @@
+function show1() {
+  var x = document.getElementById("ans1");
+  if (x.style.display === "none") {
+     x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function arrow1() {
+  var z = document.getElementById("arrow1");
+  if (z.style.transform === "none") {
+     z.style.transform = "rotate(180deg)";
+  } else {
+    z.style.transform = "none";
+  }
+}
+// document.getElementById("arrow1").addEventListener("click", function() {
+// var c = getElementById("arrow1");   
+// })
+function show2() {
+  var x = document.getElementById("ans2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+     x.style.display = "none";
+  }
+}
+function arrow2() {
+  var z = document.getElementById("arrow2");
+  if (z.style.transform === "none") {
+   z.style.transform = "rotate(180deg)";
+  } else {
+    z.style.transform = "none";
+  }
+}
+function show3() {
+  var x = document.getElementById("ans3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function arrow3() {
+  var z = document.getElementById("arrow3");
+  if (z.style.transform === "none") {
+    z.style.transform = "rotate(180deg)";
+  } else {
+    z.style.transform = "none";
+  }
+}
+function show4() {
+  var x = document.getElementById("ans4");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    } else {
+      x.style.display = "none";
+  }
+}
+function arrow4() {
+  var z = document.getElementById("arrow4");
+  if (z.style.transform === "none") {
+    z.style.transform = "rotate(180deg)";
+    } else {
+      z.style.transform = "none";
+  }
+}
